6da92e995a9915cffcc58ca987d7aae1bd0a1602
Adriano Valle <adrianovalle@mac.com>
2015-03-29 00:16:46 -0400

    so many changes... create, edit, and delete people now possible

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,8 +1,53 @@
+get '/people' do
+  @people = Person.all
+  erb :"/people/index"
+end
+
+get '/people/new' do
+  @person = Person.new
+  erb :"/people/new"
+end
+
+post '/people' do
+  if params[:birthdate].include?("-")
+    birthdate = params[:birthdate]
+  else
+    birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
+  end
+
+  person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
+
+  redirect "/people/#{person.id}"
+end
+
 get '/people/:id' do
   @person = Person.find(params[:id])
-  birthdate_string = @person.birthdate.strftime("%m%d%Y")
-  birth_path_number = Person.get_birth_path_number(birthdate_string)
+  birth_path_number = Person.get_birth_path_number(@person.birthdate.strftime("%m%d%Y"))
   @birthdate = @person.birthdate.strftime("%B %d, %Y")
   @message = Person.display_message(birth_path_number) 
   erb :"people/show"
 end
+
+get '/people/:id/edit' do
+  @person = Person.find(params[:id])
+  erb :"/people/edit"
+end
+
+put '/people/:id' do
+  # get the record and update the first_name and last_name here...
+  person = Person.find(params[:id])
+  person.first_name = params[:first_name]
+  person.last_name = params[:last_name]
+  person.birthdate = params[:birthdate]
+  person.save
+  redirect "/people/#{person.id}"
+end
+
+delete '/people/:id' do
+  person = Person.find(params[:id])
+  person.destroy
+  redirect "/people"
+end
+
+
+
