91241775679f34b37da746ea3db75edb9938b1ff
Adriano Valle <adrianovalle@mac.com>
2015-04-08 22:41:48 -0400

    got it all working again.  strftime still a problem

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -10,17 +10,38 @@ get '/people/new' do
   erb :"/people/new"
 end
 
-# post '/people' do
-#   if params[:birthdate].include?("-")
-#     birthdate = params[:birthdate]
-#   else
-#     birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
-#   end
+post '/people' do
+  if params[:birthdate].include?("-")
+    birthdate = params[:birthdate]
+  else
+    birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
+  end
 
-#   person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
+  person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
 
-#   redirect "/people/#{person.id}"
-# end
+  redirect "/people/#{person.id}"
+end
+
+get '/people/:id/edit' do
+  @person = Person.find(params[:id])
+  erb :"/people/edit"
+end
+
+put '/people/:id' do
+  # get the record and update the first_name and last_name here...
+  person = Person.find(params[:id])
+  person.first_name = params[:first_name]
+  person.last_name = params[:last_name]
+  person.birthdate = params[:birthdate]
+  person.save
+  redirect "/people/#{person.id}"
+end
+
+delete '/people/:id' do
+  person = Person.find(params[:id])
+  person.delete
+  redirect "/people"
+end
 
 # get '/people/:id' do
 #   @person = Person.find(params[:id])
@@ -28,27 +49,6 @@ end
 #   @birthdate = @person.birthdate.strftime("%B %d, %Y")
 #   @message = Person.get_message(birth_path_number) 
 #   erb :"people/show"
-# end
-
-# get '/people/:id/edit' do
-#   @person = Person.find(params[:id])
-#   erb :"/people/edit"
-# end
-
-# put '/people/:id' do
-#   # get the record and update the first_name and last_name here...
-#   person = Person.find(params[:id])
-#   person.first_name = params[:first_name]
-#   person.last_name = params[:last_name]
-#   person.birthdate = params[:birthdate]
-#   person.save
-#   redirect "/people/#{person.id}"
-# end
-
-# delete '/people/:id' do
-#   person = Person.find(params[:id])
-#   person.destroy
-#   redirect "/people"
 # end
 
 #solution code
@@ -63,36 +63,36 @@ end
 #   erb :"/people/new"
 # end
 
-post '/people' do
-  if params[:birthdate].include?("-")
-    birthdate = params[:birthdate]
-  else
-    birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
-  end
+# post '/people' do
+#   if params[:birthdate].include?("-")
+#     birthdate = params[:birthdate]
+#   else
+#     birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
+#   end
   
-  person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
-  redirect "/people/#{person.id}"
-end
+#   person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
+#   redirect "/people/#{person.id}"
+# end
 
-get '/people/:id/edit' do
-  @person = Person.find(params[:id])
-  erb :"/people/edit"
-end
+# get '/people/:id/edit' do
+#   @person = Person.find(params[:id])
+#   erb :"/people/edit"
+# end
 
-put '/people/:id' do
-  person = Person.find(params[:id])
-  person.first_name = params[:first_name]
-  person.last_name = params[:last_name]
-  person.birthdate = params[:birthdate]
-  person.save
-  redirect "/people/#{person.id}"
-end
+# put '/people/:id' do
+#   person = Person.find(params[:id])
+#   person.first_name = params[:first_name]
+#   person.last_name = params[:last_name]
+#   person.birthdate = params[:birthdate]
+#   person.save
+#   redirect "/people/#{person.id}"
+# end
 
-delete '/people/:id' do
-  person = Person.find(params[:id])
-  person.delete
-  redirect "/people"
-end
+# delete '/people/:id' do
+#   person = Person.find(params[:id])
+#   person.delete
+#   redirect "/people"
+# end
 
 get '/people/:id' do
   @person = Person.find(params[:id])
